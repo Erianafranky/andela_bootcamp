1eaa67627154daf5c77ec79998cc8ca854e3a1c6
Dorothy Ariana <arianadottie@gmail.com>
2017-03-07 05:21:35 +0300

    add testcases to prime

--- Week1/Self_Learning_Clinic/Primes/prime_numbers_test.py
+++ Week1/Self_Learning_Clinic/Primes/prime_numbers_test.py
@@ -8,7 +8,7 @@ class TestPrimeNumbers(unittest.TestCase):
 	def test_assert_input_is_not_list(self):
 		self.assertRaises(TypeError,get_primes([1,3]))
 	def test_assert_input_is_not_float(self):
-		self.assertRaises(TypeError,get_primes(5,7))
+		self.assertRaises(TypeError,get_primes(5))
 	def test_returns_correct_primes(self):
 		self.assertTrue(get_primes(20),[1,3,5,7,11,13,17,19])
 	
@@ -16,5 +16,5 @@ class TestPrimeNumbers(unittest.TestCase):
 		self.assertIs(get_primes(-10),'Please don\'t input negatives')
 	def test_returns_primes_in_array(self):
 		# Result should be an array
-		self.assertIsInstance(get_primes(20), List)
+		self.assertIsInstance(get_primes(20,[1,3,5,7,11,13,17,19])
 	
