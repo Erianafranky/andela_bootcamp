0435a2eb901720bfc5dc55f25c22a3869137ba81
Dorothy Ariana <arianadottie@gmail.com>
2017-03-08 15:41:49 +0300

    word count lab

---
+++ Week1/Remote_Day_2/Word_count.py
@@ -0,0 +1,9 @@
+def words(n):
+  result = {}
+  word_list = n.split()
+  for words in word_list:
+    if words.isdigit():
+      result [int(words)] = word_list.count(words)
+    else:
+      result[words] = word_list.count(words)
+  return result
