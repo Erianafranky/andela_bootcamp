e247f302bc5f1f5d54c068f03ffbba7d537e896d
Dorothy Ariana <arianadottie@gmail.com>
2017-03-09 15:29:35 +0300

    Command line app

--- Week1/Remote_Day_2/Http_Web/Http_Web.py
+++ Week1/Remote_Day_2/Http_Web/Http_Web.py
@@ -1,4 +1,9 @@
 import requests
 
 request = requests.get('http://anapioficeandfire.com/api/')
-print(request.text)
+print (request.status_code)
+if request.status_code != 200:
+	print ("Error: Please refresh request")
+else:
+	print (request.text)
+print (request.headers['content-type'])
